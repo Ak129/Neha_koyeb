aaa5c3ca67f98acc3019802619a563f5b3bbef10
Ak129 <64674981+Ak129@users.noreply.github.com>
2024-03-02 10:22:35 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@ then
   git clone https://github.com/Ak129/Neha_koyeb.git /Neha_koyeb
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /neha_bot
+  git clone $UPSTREAM_REPO /Neha_koyeb
 fi
-cd /neha_bot
+cd //Neha_koyeb
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
